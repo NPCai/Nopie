2781715642708a10f8d57295b0767b198838528d
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-20 15:48:36 -0400

    added to encoder model

--- model.py
+++ model.py
@@ -8,8 +8,14 @@ import torch.nn.functional as F
 class RNNEncoder(nn.Module):
 
 	def __init__(self):
+		super().__init__()
+		self.input_size = 300
+		self.hidden_size = 500
+		self.gru = nn.GRU(hidden_size, hidden_size, 1) # 1 hidden layer
+
+	def forward(self, input, hidden): # Meant to be called in a for loop over the input sequence
+		
 
 
 class RNNDecoder(nn.Module):
 
-	
