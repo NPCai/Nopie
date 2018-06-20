98db410caa295ec63043b878114a96bc7d250e9c
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-20 15:12:23 -0400

    added template format

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
-import 
+import torch
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+import torch.nn.functional as F
+
+class RNNEncoder(nn.Module):
+
+	def __init__(self):
+
+
+class RNNDecoder(nn.Module):
+
+	
