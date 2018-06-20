28d9c87ae38c2690ecbba989708d1322dcbf68fd
TheodoreChristakis <theodorechristakis@gmail.com>
2018-06-20 16:38:06 -0400

    Train.py initial commit

--- src/train.py
+++ src/train.py
@@ -1,6 +1,10 @@
+import model
 import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
-def train(input)
+def train(input, target, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
+	encoder_hidden = encoder.initHidden()
+
+print("PooPoo")
