fbd31e2ce4853f18c1b6784818ed15a79527b13d
TheodoreChristakis <theodorechristakis@gmail.com>
2018-06-20 15:46:30 -0400

    DecoderRNN

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 import torch
 import torch
 import torch.nn as nn
@@ -11,5 +14,21 @@ class RNNEncoder(nn.Module):
 
 
 class RNNDecoder(nn.Module):
+	def __init__(self, hidden_size, output_size):
+		super(RNNDecoder, self).__init__()
+		self.hidden_size = hidden_size
 
-	
+		self.embedding = nn.Embedding
+		self.gru = nn.GRU(hidden_size, hidden_size)
+		self.out = nn.Linear(hidden_size, output_size)
+		self.softmax = nn.LogSoftmax(dim=1)
+
+	def forward(self, input, hidden):
+		output = self.embedding(input).view(1,1,-1)
+		output = F.relu(output)
+		output, hidden = self.gru(output,hidden)
+		output = self.softmax(Self.out(output[0]))
+		return output, hidden
+
+	def initHidden(self):
+		return torch.zeros(1,1, self.hidden_size, device = device)
