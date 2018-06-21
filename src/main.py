0072228a5f1f5835936e121d62350decb4bf59a5
TheodoreChristakis <theodorechristakis@gmail.com>
2018-06-21 16:53:59 -0400

    main changes from test.py

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 from model import RNNEncoder, RNNDecoder
+from train import RNN, train, evaluation, save
 
 def main():
-
+	data = "This is a sample sentence" # Pass the data through here later
+	rnn = RNN(data.input_size, data.output_size)
 	encoder = RNNEncoder()
 	decoder = RNNDecoder()
 
