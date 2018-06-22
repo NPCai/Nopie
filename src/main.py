3ad3fca254d4be4dbc94ffcc01d9b7e5b4ed05a2
TheodoreChristakis <theodorechristakis@gmail.com>
2018-06-22 13:14:59 -0400

    main.py additions

--- src/main.py
+++ src/main.py
@@ -6,12 +6,25 @@ import torch.nn.functional as F
 from model import RNNEncoder, RNNDecoder
 from train import RNN, train, evaluation, save
 
+
+dataSize = 1000
+
 def main():
 	data = "This is a sample sentence" # Pass the data through here later
 	rnn = RNN(data.input_size, data.output_size)
 	encoder = RNNEncoder()
 	decoder = RNNDecoder()
 
+	catchingLs = []
+	for i, batch in sentences:
+		input, target = batch
+
+		loss = rnn.train(input, target)
+		catchingLs.append(loss)
+
+		if i % 100 is 0:
+			print("Loss at epoch %d: %.2f" % (i, loss))
+			rnn.save()
 	
 
 
