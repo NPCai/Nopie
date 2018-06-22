b8be3b9d7f0db912c7adfce1e0c7ad222f08b6cb
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-22 15:28:28 -0400

    added string2vec function for converting sequences from glove vectors

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import csv
 import torch
+import spacy
 
+nlp = spacy.load('en')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 words = pd.read_table("../data/glove_100d.txt", sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
 
@@ -10,18 +12,23 @@ def glove(word):
 	try:
 		v = torch.tensor(words.loc[word].values, requires_grad=False, device=device) # Don't update embeddings
 	except KeyError:
-		v = torch.zeros(50)
+		v = torch.zeros(100)
 	return v
 
-def sentence2vec(sentence):
-	pass
-	''' Takes in a sentence string and produces a variable-length vectorization augmented with spacy dep and pos'''
+def string2vec(sentence):
+	''' Takes in a sentence string and produces a variable-length vectorization'''
+	# TODO(jacob) augment with spacy pos and dep data
+	doc = nlp(sentence) # segment the sentence
+	vecs = []
+	for token in doc:
+		vecs.append(glove(token.lower_))
+	return torch.tensor(torch.stack(vecs)) # Have to stack so the tensors are not on the inside
 
-def tuple2vec(tups):
-	''' Takes in a tuple and produces a variable-length vectorization '''
+def vec2string():
 	pass
+	# TODO(jacob)
 
 if __name__ == "__main__": # For testing purposes
 	print("ready")
 	while True:
-		print(glove(input())) 
+		print(string2vec(input())) 
