827fd380167696182208a9dd296b95ed67a25743
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-21 22:33:14 -0400

    naming convention changes in training

--- src/train.py
+++ src/train.py
@@ -7,43 +7,37 @@ import torch.nn.functional as F
 SOS_token = 0 # Start of sentence
 EOS_token = 1 # End of sentence
 
-class RNN(object):
+class RNN():
+
 	def __init__(self, input_size, output_size):
-		super(RNN, self).__init__()
-
-		self.encoder = RNNEncoder(input_size) # Defining the encoder
-		self.decoder = RNNDecoder(output_size) # Defining the decoder
-
-		self.loss = nn.CrossEntropyLoss() # Loss function also known as log loss
-		self.encoder_optimizer = optim.Adam(self.encoder.parameters()) # Adam optimizer is a stochastic gradient descent which maintains a single learning rate
-																	   # for all weight updates and does not change during training. A learning rate is maintained
-																	   # for each network weight and separately adapted as learning progresses
+		super().__init__()
+		self.encoder = RNNEncoder(input_size) 
+		self.decoder = RNNDecoder(output_size)
+		self.lossFn = nn.CrossEntropyLoss()
+		self.encoder_optimizer = optim.Adam(self.encoder.parameters()) 												   
 		self.decoder_optimizer = optim.Adam(self.decoder.parameters())
 
 		sos, eos = torch.LongTensor(1, 1).zero_(), torch.LongTensor(1, 1).zero_()
 		sos[0,0], eos[0,0] = 0, 1 # Defines the SOS and EOS as 1x1 tensors with values 0 and 1 respectively
-
 		self.sos, self.eos = sos, eos
 
-
-	def train(self, input, target):
-		self.encoder_optimizer.zero_grad() # Zeros the gradient before every iteration
+	def train(self, seqIn, seqOut): 
+		''' Train one iteration, no batch '''
+		self.encoder_optimizer.zero_grad() 
 		self.decoder_optimizer.zero_grad()
 
-		# Encoder stuff
-		_, encoder_hidden = self.encoder.forward(1, encoder_hidden) # Forward propogation of the encoder to the decoder
+		hidden = self.encoder.forward(1, seqIn) # Encode sentence
 
 		# Decoder stuff
-		target.insert(0, self.sos) # Insert the start of sentence token into the first position
-		target.append(self.eos) # Append the end of sentence token into the end
+		seqOut.insert(0, self.sos) # Insert the start of sentence token into the first position
+		seqOut.append(self.eos) # Append the end of sentence token into the end
 		loss = 0
-		for i in range(len(target) - 1):
-			_, softmax, encoder_hidden = self.decoder.forward(target[i], encoder_hidden)
-			loss += self.loss(softmax, target[i+1][0])
+		for i in range(len(seqOut) - 1):
+			_, softmax, hidden = self.decoder.forward(seqOut[i], hidden)
+			loss += self.lossFn(softmax, seqOut[i+1][0])
 
-		loss.backward()
-
-		self.encoder_optimizer.step()
+		loss.backward() # Compute grads with respect to the network
+		self.encoder_optimizer.step() # Update using the stored grad
 		self.decoder_optimizer.step()
 		return loss.data[0]
 
@@ -53,7 +47,6 @@ class RNN(object):
 
 		sentence = []
 		input = self.sos
-
 
 	def save(self):
 		torch.save(self.encoder.state_dict(), "encoder.ckpt")
