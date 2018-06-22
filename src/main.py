e0253ba51bd96dddb5ae38d2e7c4dd087193af1c
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-22 13:42:28 -0400

    fixed default and data loader issue

--- src/main.py
+++ src/main.py
@@ -4,27 +4,18 @@ from torch import optim
 import torch.nn.functional as F
 from model import Encoder, Decoder
 from train import RNN
-import pandas as pd
-import csv
 
-words = pd.read_table("../data/glove50d.txt", sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
-
-def vec(w):
-  return words.loc[w].as_matrix()
 
 
 dataSize = 1000
 
 def main():
-	print("ready")
-	while True:
-		print(vec(input()))
 
 	data = "This is a sample sentence" # Pass the data through here later
 	rnn = RNN(data.input_size, data.output_size)
 	encoder = RNNEncoder()
 	decoder = RNNDecoder()
-	
+
 	catchingLs = []
 	for i, batch in sentences:
 		input, target = batch
---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import csv
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+words = pd.read_table("../data/glove50d.txt", sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
+
+def vec(word):
+	v = None
+	try:
+		v = torch.tensor(words.loc[word].values, requires_grad=False, device=device) # Don't update embeddings
+	except KeyError:
+		v = torch.zeros(50)
+	return v
+
+def sentence_vec(sentence):
+	''' Takes in a spacy sentence object and produces 
+
+if __name__ == "__main__": # For testing purposes
+	print("ready")
+	while True:
+		print(vec(input()))
