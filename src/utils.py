aeccf3695d0e4a6ea6009ba485be8818bea62609
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-25 13:35:53 -0400

    refactoring and removing vec2text code

--- src/utils.py
+++ src/utils.py
@@ -4,42 +4,24 @@ import torch
 import spacy
 import wordvecs
 
+nlp = spacy.load('en')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+words = pd.read_table("../data/glove_100d.txt", sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
+
 def word2vec(word):
 	''' Converts a string to a vector using GloVe '''
 	v = None
 	try:
-		v = torch.tensor(wordvecs.words.loc[word].values, requires_grad=False, device=wordvecs.device) # Don't update embeddings
+		v = torch.tensor(words.loc[word].values, requires_grad=False, device=device) # Don't update embeddings
 	except KeyError:
 		v = torch.zeros(100)
 	return v
 
-def vec2word(vec): # Expects a torch tensor
-	w = ""
-	try:
-		w = wordvecs.vecs[str(vec)]
-	except KeyError:
-		w = ""
-	return w	
-
 def string2vec(sentence):
 	''' Takes in a sentence string and produces a variable-length vectorization'''
 	# TODO(jacob) augment with spacy pos and dep data
-	doc = wordvecs.nlp(sentence) # segment the sentence
+	doc = nlp(sentence) # segment the sentence
 	vecs = []
 	for token in doc:
 		vecs.append(word2vec(token.lower_))
 	return torch.tensor(torch.stack(vecs), requires_grad=False) # Have to stack so the tensors are not on the inside
-
-def vec2string(vectors):
-	sentence = []
-	for vector in vectors:
-		print(vector)
-		sentence.append(vec2word(vector))
-	return sentence	
-
-
-if __name__ == "__main__": # For testing purposes, convert into seq then back to words
-	print("ready")
-	while True:
-		x =  string2vec(input())
-		print(vec2string(x))
