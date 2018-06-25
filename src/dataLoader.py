824c5617a20eace3add5f4a86d7aa2a488274dbe
TheodoreChristakis <theodorechristakis@gmail.com>
2018-06-25 12:20:33 -0400

    dataLoader.py initial commit

---
+++ src/dataLoader.py
@@ -0,0 +1,17 @@
+import json
+import sys
+import numpy as np
+
+TRAINFILE = "../data/tuples-train.json"
+OUTPUT_FILE = "../data/tuples-output-train.json"
+
+if len(sys.argv) > 1 and sys.argv[1] == "dev":
+	TRAINFILE = "../data/tuples-dev.json"
+	OUTPUT_FILE = "../data/tuples-output-dev.json"
+
+def gimme_Data(sentence, tuple, reverse=False):
+	print("Extracting...", "\n")
+	with open(TRAINFILE, encoding = "utf8") as f:
+		json_set = json.load(f)
+		dataset = json_set['data']
+
--- src/main.py
+++ src/main.py
@@ -7,13 +7,8 @@ from train import RNN
 from utils import word2vec, vec2word, string2vec, vec2string
 import wordvecs
 import json
+import dataLoader
 
-
-JSON_FILE = "../data/tuples-dev.json"
-
-def gimme_Data(sentence, tuple, reverse=False):
-	print("Extracting...")
-	
 
 def main():
 	data = "This is a sample sentence" # Pass the data through here later
