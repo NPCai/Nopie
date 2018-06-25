de11d7c638577bcdba24d863bb33f03622a64d92
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-25 13:07:17 -0400

    added epochs still todo randomization

--- src/main.py
+++ src/main.py
@@ -2,43 +2,27 @@ import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
-from model import RNNEncoder, RNNDecoder
-from train import RNN
-from utils import word2vec, vec2word, string2vec, vec2string
-from wordvecs import *
-import json
+from train import EncoderDecoder
 import dataLoader
+import utils
 
-torch.set_default_tensor_type(torch.FloatTensor)
+ed = EncoderDecoder()
 
-def main():
-	ed = EncoderDecoder()
-	data = dataLoader.pairs(devSet=True)
+data = dataLoader.pairs(devSet=True)
+for epoch in range(5): 
+	for pair in data: # TODO(jacob), batches, randomization
+		seqIn = utils.seq2vec(pair['sentence'])
+		seqOut = [ed.sos]
+		for tup in pair['tuples']:
+			seqOut.append(utils.seq2vec[tup])
+		seqOut.append(ed.eos)
+		seqOut = torch.stack(torch.tensor(seqOut))
+		loss += ed.train(seqIn, seqOut)
 
-	for epoch in range(5):
-		for pair in data: # TODO(jacob), batches
-			seqIn = utils.seq2vec(pair['sentence'])
-			seqOut = [ed.sos]
-			for tup in pair['tuples']:
-				seqOut.append(utils.seq2vec[tup])
-			seqOut.append(ed.eos)
-			seqOut = torch.stack(torch.tensor(seqOut))
-			loss += ed.train(seqIn, seqOut)
+	print("Total loss at epoch %d: %.2f" % (epoch, loss))
+	print("Saved", "\n")
+ed.save()
 
-		print("Total loss at epoch %d: %.2f" % (epoch, loss))
-		print("Saved", "\n")
-	ed.save()
-
-def finalOutput():
-	sentence = "This is a sample sentence" # Pass data through later
-	data = glove(sentence)
-	rnn = RNN(data.input_size, data.output_size)
-	vectors = data.string2vec("This is a sample sentence <EOS>")
-
-	finalOutput = rnn.evaluation(vectors)
-	print(data.vec2string(finalOutput))
-
-
-if __name__ == "__main__":
-	main()
-	finalOutput()
+sentence = seq2vec(input())
+finalOutput = rnn.evaluation(vectors)
+print(data.vec2string(finalOutput))
