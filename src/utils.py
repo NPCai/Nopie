a6854e09e53af28fe5296ed8213dd433146335c5
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-06-25 14:06:05 -0400

    fixed bad merge in utils

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import spacy
 nlp = spacy.load('en')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 words = pd.read_table("../data/glove_100d.txt", sep=" ", index_col=0, header=None, quoting=csv.QUOTE_NONE)
-numToWord = enumerate(words.index.values)
+numToWord = dict(enumerate(words.index.values)) # create mapping from unique int to word for decoding
 
 def word2vec(word):
 	''' Converts a string to a vector using GloVe '''
@@ -26,21 +26,6 @@ def string2vec(sentence):
 	for token in doc:
 		vecs.append(word2vec(token.lower_))
 	return torch.tensor(torch.stack(vecs), requires_grad=False).float() # Have to stack so the tensors are not on the inside
-
-def vec2string(vectors):
-	sentence = []
-	for vector in vectors:
-		print(vector)
-		sentence.append(vec2word(vector))
-	return sentence	
-
-
-if __name__ == "__main__": # For testing purposes, convert into seq then back to words
-	print("ready")
-	while True:
-		x =  string2vec(input())
-		print(vec2string(x))
-	return torch.tensor(torch.stack(vecs), requires_grad=False) # Have to stack so the tensors are not on the inside
 
 def num2word(num):
 	if num < 0 or num >= len(numToWord):
