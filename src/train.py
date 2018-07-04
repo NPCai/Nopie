6b392d083ca7437bdd10e0a4a46a712b7bac1725
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-07-04 15:22:44 -0400

    trying getting rid of seq prob

--- src/train.py
+++ src/train.py
@@ -84,7 +84,7 @@ class EncoderDecoder():
 		total_reward = self.critic.forward(sentence, tup_str)
 		print("RL STEP", torch.stack(log_probs).sum())
 		seq_prob = torch.stack(log_probs).sum() / 10
-		loss = seq_prob * total_reward
+		loss = (seq_prob / seq_prob ) * total_reward
 		before = time.time()
 		loss.backward() # Compute grads with respect to the network
 		self.encoder_optimizer.step() # Update using the stored grad
