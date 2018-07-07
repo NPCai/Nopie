2b90e740785a06c23a29d3b5550072b1fd0ec9e8
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-07-07 12:23:01 -0400

    up lr

--- src/train.py
+++ src/train.py
@@ -31,8 +31,8 @@ class EncoderDecoder():
 		self.attndecoder = RNNAttentionDecoder().to(device)
 		self.lossFn = nn.CrossEntropyLoss()
 		self.critic = customLoss.TupleCritic()
-		self.encoder_optimizer = optim.Adam(self.encoder.parameters(), lr=1e-3) 												   
-		self.decoder_optimizer = optim.Adam(self.decoder.parameters(), lr=1e-3)
+		self.encoder_optimizer = optim.Adam(self.encoder.parameters(), lr=1e-2) 												   
+		self.decoder_optimizer = optim.Adam(self.decoder.parameters(), lr=1e-2)
 
 	def train(self, seqIn, seqOutOneHot, seqOutEmbedding): 
 		''' Train one iteration, no batch '''
