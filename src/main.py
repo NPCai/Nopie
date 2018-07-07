e1e63625612d0adae346d3603e3e069d76a5c70a
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-07-07 10:52:09 -0400

    more isolation

--- src/main.py
+++ src/main.py
@@ -39,7 +39,7 @@ for batch in range(batchRange):
 	#for i in np.random.randint(len(data), size=5):
 	for i in np.random.randint(13, size=5):
 		#minibatch.append(data[i])
-		minibatch.append({'sentence': str([j for j in range(i,i+5)])})
+		minibatch.append({'sentence': str([j for j in range(i%10,(i+5)%10)])})
 	batchSeqIn = []
 	batchSeqOutOneHot = []
 	batchSeqOutEmbedding = []
