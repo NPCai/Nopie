12096566dfb42e9bd5e9a76e92490ccafdaa335b
Jacob Beckerman <jbecke@seas.upenn.edu>
2018-07-09 14:37:26 -0400

    asd

--- src/main.py
+++ src/main.py
@@ -40,7 +40,7 @@ for batch in range(batchRange):
 	before = time.time()
 	for i in np.random.randint(5, size=10):
 		#minibatch.append(data[i])
-		x = [j for j in range(5,i+4+i)]
+		x = [j for j in range(1,5)]
 		y = ""
 		for i in x:
 			y += str(i) + ' '
